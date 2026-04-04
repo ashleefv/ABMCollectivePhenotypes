9911ba24926d75e1734677c827d574d8ba4190ae
Benson <tbenson2@buffalo.edu>
2026-04-04 11:10:13 -0400

    simulation code

---
+++ CellPhenotype_cc3d_baseline_singleRun/Simulation/CellPhenotype.py
@@ -0,0 +1,19 @@
+
+from cc3d import CompuCellSetup
+        
+
+
+from CellPhenotypeSteppables import ConstraintInitializerSteppable
+
+CompuCellSetup.register_steppable(steppable=ConstraintInitializerSteppable(frequency=1))
+
+
+
+
+from CellPhenotypeSteppables import MitosisSteppable
+
+CompuCellSetup.register_steppable(steppable=MitosisSteppable(frequency=1))
+
+
+
+CompuCellSetup.run()
