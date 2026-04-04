ea132f5e8ff1090c54566f908b60b3a958546878
Temitope O. Benson <68304751+temitope-benson@users.noreply.github.com>
2026-04-04 10:12:44 -0400

    updated version

--- CellPhenotype.py
+++ CellPhenotype.py
@@ -10,16 +10,10 @@ CompuCellSetup.register_steppable(steppable=ConstraintInitializerSteppable(frequ
 
 
 
-# from CellPhenotypeSteppables import GrowthSteppable
-
-# CompuCellSetup.register_steppable(steppable=GrowthSteppable(frequency=1))
-
-
-
-
 from CellPhenotypeSteppables import MitosisSteppable
 
 CompuCellSetup.register_steppable(steppable=MitosisSteppable(frequency=1))
+
 
 
 CompuCellSetup.run()
